f0eb9ab600f95881b8471b9cc84407830c5b65fa
jaferDevEngineer <jafer.devengineer@gmail.com>
2025-08-29 10:23:11 +0530

    new solutions

--- src/DSA/FindLongestSubstring.java
+++ src/DSA/FindLongestSubstring.java
@@ -1,27 +1,29 @@
 package DSA;
 
+import java.util.Arrays;
+
 public class FindLongestSubstring {
 
 	public static void main(String[] args) {
 		String s = "abacdc";
-		System.out.println(findLongestSubstring(s));
+		System.out.println(findLongestSubstringV2(s));
 	}
 
-	private static int findLongestSubstring(String s) {
-		int start = 0;
-		int max = 0;
-		int[] lastSeen = new int[256];
+	private static int findLongestSubstringV2(String s) {
 
-		for (int i = 0; i < s.length(); i++) { // 0 //1 //2 //3 //4 //5
-			char currentCharacter = s.charAt(i); // a //b //a //c //d //c
-			if (lastSeen[currentCharacter] > start) {// false //false // true //false //false // true
-				start = lastSeen[currentCharacter];// // // start=1 // // start=4
+		int start = 0, max = 0;
+
+		int[] arr = new int[256];
+		for (int i = 0; i < s.length(); i++) {
+			char current = s.charAt(i);
+			if (arr[current] > start) {
+				start = arr[current];
 			}
-			lastSeen[currentCharacter] = i + 1; // a=1 //b=2 //a=3 //c=4 // d= 5 // c= 6
-			max = Math.max(max, i + 1 - start); // Math.max(0,0+1 - 0) = 1 // Math.max(1,1+1 - 0) = 2 // Math.max(2,2+1
-												// - 1) = 2 // Math.max(2,3+1 - 1) = 3 // Math.max(3,4+1 - 1) =4
-												// ,//Math.max(4,5+1 - 1) =4
+			arr[current] = i + 1;
+			max = Math.max(max, i + 1 - start);
+
 		}
+
 		return max;
 	}
 }
--- src/DSA/LongestPalindrome.java
+++ src/DSA/LongestPalindrome.java
@@ -2,11 +2,74 @@ package DSA;
 
 public class LongestPalindrome {
 	public static void main(String[] args) {
-		String d = "";
-		new LongestPalindrome().longestPolindrome(d);
+		String d = "bb";
+//		System.out.println(d.substring(1, 3));
+//		System.out.println(new LongestPalindrome().longestPolindrome(d));
+		System.out.println(new LongestPalindrome().longestPolindromeV2(d));
 	}
 
-	private void longestPolindrome(String d) {
-		
+	private String longestPolindrome(String s) {
+		 if (s == null || s.length() < 2) return s; // "" or single char
+
+	        int start = 0, end = 0; // best window [start..end]
+
+	        for (int i = 0; i < s.length(); i++) {
+	            // Expand for odd-length palindromes (center at i)
+//	        	System.out.println("s "+);
+	            int len1 = expand(s, i, i);
+
+	            // Expand for even-length palindromes (center between i and i+1)
+	            int len2 = expand(s, i, i + 1);
+	            System.out.println("len1 "+len1 + " len2 "+len2);
+
+	            int len = Math.max(len1, len2);
+
+	            // If this center gives a longer palindrome, update window
+	            if (len > end - start + 1) {
+	            	System.out.println("in");
+	                // map center i and length len to [start..end]
+	                start = i - (len - 1) / 2;
+	                end   = i +  len      / 2;
+	            }
+	            System.out.println();
+	        }
+	        System.out.println(start+" "+end);
+	        return s.substring(start, end + 1);
+	}
+	 private int expand(String s, int L, int R) {
+	        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {//  l =1,r =1
+	            L--; // -1 
+	            R++; //  1
+	        }
+	        // when while breaks, [L+1..R-1] is last valid palindrome
+	        System.out.println(R+" - " + L +" - "+ 1+" "+ (  R - L - 1));
+	        return R - L - 1; // 3 - -1 -1 
+	    }
+
+	private String longestPolindromeV2(String s) {
+		int start = 0, end = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int len = frontAndBackPalindrome(s, i, i);
+			int len2 = frontAndBackPalindrome(s, i, i + 1);
+
+			int highest = Math.max(len, len2);
+
+			if (highest > end - start + 1) {
+				start = i - (highest - 1) / 2;
+				end = i + highest / 2;
+//				System.out.println(start+" "+end);
+			}
+		}
+
+        System.out.println(start+" "+end);
+		return s.substring(start, end+1);
+	}
+
+	private int frontAndBackPalindrome(String s, int l, int r) {
+		while (l >= 0 && r < s.length() && s.charAt(r) == s.charAt(l)) {
+			l--;
+			r++;
+		}
+		return r - l - 1;
 	}
 }
